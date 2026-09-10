4b6a9b5e5e8c1dd65501cfbc416195f6af26e09b
Igor Tron <root@itron.pro>
2026-09-10 22:17:03 +0400

    Fix PHP 8.4 nullable deprecations

--- src/Exceptions/wppaCreatePostException.php
+++ src/Exceptions/wppaCreatePostException.php
@@ -11,7 +11,7 @@ class wppaCreatePostException extends wppaException implements wpException {
 
 	public $error;
 
-	function __construct( wpPostAble $postable, WP_Error $error, $message = "", $code = 0, Throwable $previous = null ) {
+	function __construct( wpPostAble $postable, WP_Error $error, $message = "", $code = 0, ?Throwable $previous = null ) {
 		parent::__construct( $postable, $message, $code, $previous );
 		$this->error = $error;
 	}
